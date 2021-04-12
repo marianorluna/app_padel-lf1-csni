0e4a44caf84a68ce03acec369bd64305016a0552
marianorluna <marianorluna@gmail.com>
2021-04-12 23:12:38 +0200

    upload v1.2.6

--- data.js
+++ data.js
@@ -998,11 +998,11 @@ let resGamesA2 = [
     {
         //Partido P14
         //Set1
-        s1_t1: 0,
-        s1_t2: 0,
+        s1_t1: 6,
+        s1_t2: 3,
         //Set2
-        s2_t1: 0,
-        s2_t2: 0,
+        s2_t1: 6,
+        s2_t2: 1,
         //Set3
         s3_t1: 0,
         s3_t2: 0
@@ -1010,11 +1010,11 @@ let resGamesA2 = [
     {
         //Partido P15
         //Set1
-        s1_t1: 6,
-        s1_t2: 3,
+        s1_t1: 0,
+        s1_t2: 0,
         //Set2
-        s2_t1: 6,
-        s2_t2: 1,
+        s2_t1: 0,
+        s2_t2: 0,
         //Set3
         s3_t1: 0,
         s3_t2: 0
