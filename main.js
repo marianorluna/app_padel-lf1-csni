abb19832e9507b00c91cc588b9313a7a0a7792d4
marianorluna <marianorluna@gmail.com>
2021-05-29 13:14:47 +0200

    upload v1.2.29

--- main.js
+++ main.js
@@ -2067,7 +2067,7 @@ let txtMarque = document.getElementById("text-news");
 function cambiarMarque() {
     //txtMarque.href = "https://www.worldpadeltour.com/";
     //txtMarque.innerHTML = "WPT | Adeslas Madrid Open | Finales | Streaming LINK ==>";
-    txtMarque.innerHTML = "CUADRO DE OCTAVOS DE FINAL";
+    txtMarque.innerHTML = "CUADRO FINAL";
 }
 // IMPORTANTE PARA LOS CAMBIOS!!!
 window.addEventListener("load", cambiarMarque() );      //cambia texto de marquesina
