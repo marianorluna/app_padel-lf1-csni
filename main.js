efd69b7892103dd0807fa123fbea6d0528576330
marianorluna <marianorluna@gmail.com>
2021-06-29 02:55:33 +0200

    upload v1.2.33

--- main.js
+++ main.js
@@ -2037,7 +2037,7 @@ function textFinalA() {
 window.addEventListener("load", includeRes8A() );      //inners res 8vosA
 window.addEventListener("load", includeRes4A() );      //inners res 4vosA
 window.addEventListener("load", includeRes2A() );      //inners res 2vosA
-// window.addEventListener("load", includeRes1A() );      //inners res 1vosA
+window.addEventListener("load", includeRes1A() );      //inners res 1vosA
 //window.addEventListener("load", textFinalA() );        //añade texto win A
 
 // Resaltar los partidos JUGADOS
